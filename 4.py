80a05db9535c6bd8fc39c7c3daec75722d8a58cd
LevRozengart <150509658+LevRozengart@users.noreply.github.com>
2023-11-18 13:06:56 +0500

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,17 @@
+a = 'ds  ad   adsa     d'
+array = []
+def del_space(a):
+    a =  a.lower()
+    lst_char1 = ''
+    lst_char2 = ''
+    i = 0
+    for char in a:
+        lst_char2 = lst_char1
+        lst_char1 = char
+        if lst_char1 == lst_char2 and lst_char1 == ' ':
+            a = a[:i] + a[i+1:]
+        else:
+            i += 1
+    print(a)
+    return a
+del_space(a)
