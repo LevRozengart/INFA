d6ec8940884a41a675785f7ccf4099a215475daf
LevRozengart <150509658+LevRozengart@users.noreply.github.com>
2023-12-23 20:52:57 +0500

    Add files via upload

---
+++ tasks 2/7.py
@@ -0,0 +1,10 @@
+def generate_message(a):
+    res = ""
+    dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
+    for char in a:
+        if char.upper() in dict:
+            i = dict[char.upper()]
+            res += char * i
+    print(res)
+    return res
+
